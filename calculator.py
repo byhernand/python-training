d2172b6b20e84a98d8bbc172ea088816d0101884
Eduardo Hernand <byhernand@hotmail.com>
2024-02-08 07:09:29 -0600

    Calculator script

---
+++ calculator.py
@@ -0,0 +1,19 @@
+def add(a, b):
+  print(a + b)
+
+def subtract(a, b):
+  print(a - b)
+
+def multiply(a, b):
+  print(a * b)
+
+def divide(a, b):
+  print(a / b)
+
+
+# Testing
+
+add(34, 21)
+subtract(78, 42)
+multiply(5, 6)
+divide(84, 7)
